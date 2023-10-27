bfe73cd215be866cffeaea6fe0d0b4b0b20f764f
crown <110808872+yqunf@users.noreply.github.com>
2023-10-27 14:48:41 +0800

    Add files via upload

---
+++ codes/Gm-AAC.py
@@ -0,0 +1,38 @@
+import re
+
+def GmAAC(fastas):
+    group = {
+        'A': 'A',
+        'C': 'CDHNQST',
+        'G': 'GPV',
+        'E': 'E',
+        'F': 'FIMY',
+        'K': 'K',
+        'L': 'LW',
+        'R': 'R'
+    }
+
+    groupKey = group.keys()
+
+    encodings = []
+    header = ['#']
+    for key in groupKey:
+        header.append(key)
+    encodings.append(header)
+
+    for i in fastas:
+        name, sequence = i[0], re.sub('-', '', i[1])
+        code = [name]
+        count = Counter(sequence)
+        myDict = {}
+        for key in groupKey:
+            for aa in group[key]:
+                myDict[key] = myDict.get(key, 0) + count[aa]
+
+        for key in groupKey:
+            code.append(myDict[key] / len(sequence))
+        encodings.append(code)
+
+    return encodings
+
+
